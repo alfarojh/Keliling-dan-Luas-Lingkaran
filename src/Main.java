98d14f64fcc8aefdc6b80d2b6f36919f24d707a1
alfaro <alfarous.jh@gmail.com>
2023-07-24 13:10:20 +0700

    Penambahan comment

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,7 @@ public class Main {
     public static double kelilingLingkaran(double jari){
         return 2 * Math.PI * jari;
     }
+
     // Menghitung luas lingkaran
     public static double luasLingkaran(double jari){
         return Math.PI * jari * jari;
